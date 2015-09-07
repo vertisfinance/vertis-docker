b46d4268487fcabd9b626d81a47a873163b8f155
Richard Bann <richard.bann@vertis.com>
2015-09-07 18:40:28 +0200

    runutils as python module

--- buildall.sh
+++ buildall.sh
@@ -1,4 +1,5 @@
 docker build -t vertisfinance/base vertisfinance-base
 docker build -t vertisfinance/postgres vertisfinance-postgres
-docker build -t vertisfinance/django-python3 vertisfinance-django-python3
 docker build -t vertisfinance/nginx vertisfinance-nginx
+docker build -t vertisfinance/django-python2 vertisfinance-django-python2
+docker build -t vertisfinance/django-python3 vertisfinance-django-python3
